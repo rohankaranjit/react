a4aaa68100d59a01e467186aa73d0526e20ad258
Rohan Karanjit <109459345+rohankaranjit@users.noreply.github.com>
2024-09-10 23:45:01 +0545

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import "./index.css"
+
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="calculator-grid">
+      <div className="output">
+      <div className="previous-operand">34123212341 * </div>
+      <div className="current-operand">123132131232212132121321312131</div>
+      </div>
+      <button className="span-two">AC</button>
+      <button>DEL</button>
+      <button>÷</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button className="span-two">=</button>
+    </div>
+  );
+}
