dc746be0d65c965fa5295cf8ab491f8437601a9c
Rohan Karanjit <109459345+rohankaranjit@users.noreply.github.com>
2024-09-11 23:24:01 +0545

    Update Operationbutton.js

--- src/Operationbutton.js
+++ src/Operationbutton.js
@@ -1,1 +1,9 @@
+import React from 'react'
+import { ACTIONS } from './App'
+
+export default function OperationButton({dispatch, digit}) {
+return (
+  <button onClick={()=> dispatch({type:ACTIONS.CHOOSE_OPERATION, payload : {digit}})}>{digit}</button>
+)
+}
 
