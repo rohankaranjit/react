6d6076e53056a3a308ab710420fb5f4c92b5b982
Rohan Karanjit <109459345+rohankaranjit@users.noreply.github.com>
2024-09-15 22:03:59 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import DigitButton from "./DigitButton"
 import OperationButton from "./OperationButton"
 import "./App.css"
 
-
 export const ACTIONS = {
   ADD_DIGIT: "add-digit",
   CHOOSE_OPERATION: "choose-operation",
